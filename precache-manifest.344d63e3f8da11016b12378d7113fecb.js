f9de5d93a9d91ebd57c73fced4486fd27e8deb13
ssingh <sandy.boon@gmail.com>
2020-07-03 23:11:12 -0400

    Updates

---
+++ precache-manifest.344d63e3f8da11016b12378d7113fecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881e4c0f2c1f5e733f14754b91ca3455",
+    "url": "/deployed-react-portfolio/index.html"
+  },
+  {
+    "revision": "f00c8e6facdb2c59e588",
+    "url": "/deployed-react-portfolio/static/css/main.18ca97b4.chunk.css"
+  },
+  {
+    "revision": "dab31ecd26dfef726198",
+    "url": "/deployed-react-portfolio/static/js/2.4be7b80c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/deployed-react-portfolio/static/js/2.4be7b80c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00c8e6facdb2c59e588",
+    "url": "/deployed-react-portfolio/static/js/main.1fd46c8f.chunk.js"
+  },
+  {
+    "revision": "b6a4c682620fdfd2c26b",
+    "url": "/deployed-react-portfolio/static/js/runtime-main.2bb77ab0.js"
+  }
+]);
